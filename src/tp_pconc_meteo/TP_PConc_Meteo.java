23e363f95ad2568c7c1b3c606bbe698a24724540
skl <skl@macbookskl.einet.ad.eivd.ch>
2014-05-19 20:32:38 +0200

    first commit

--- src/tp_pconc_meteo/TP_PConc_Meteo.java
+++ src/tp_pconc_meteo/TP_PConc_Meteo.java
@@ -11,12 +11,13 @@ package tp_pconc_meteo;
  * @author skl
  */
 public class TP_PConc_Meteo {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("heleo world!");
+    
     }
     
 }

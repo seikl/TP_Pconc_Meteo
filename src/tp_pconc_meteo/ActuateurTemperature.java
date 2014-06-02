fb1b833260b3e3eb0785cc108169e77d80b25cce
jit <jithurbide@gmail.com>
2014-06-02 21:05:01 +0200

    Merge origin/master
    Conflicts:
    	src/tp_pconc_meteo/Le_Main.java

---
+++ src/tp_pconc_meteo/ActuateurTemperature.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tp_pconc_meteo;
+
+/**
+ *
+ * @author jit
+ */
+
+
+public class ActuateurTemperature {
+    
+    private SC_Temperature scTemp_;
+    public boolean readyToWrite_ = true;
+    private double tempAModifier_;
+    
+    ActuateurTemperature()
+    {
+        tempAModifier_=0;
+    }
+    
+    public void setTempToModify (double temp)
+    {
+        tempAModifier_ = temp;
+    }
+    public double getTempToModify()
+    {
+        return tempAModifier_;
+    }
+    
+}
---
+++ src/tp_pconc_meteo/CapteurTemperature.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tp_pconc_meteo;
+
+/**
+ *
+ * @author jit
+ */
+public class CapteurTemperature {
+    
+    private SC_Temperature scTemp_;
+    public boolean readyToWrite_ = true;
+    private double temp_;
+    
+    CapteurTemperature()
+    {
+        temp_=0;
+    }
+    
+    public void setTemp (double temp)
+    {
+        temp_ = temp;
+    }
+    public double getTemp()
+    {
+        return temp_;
+    }
+    
+}

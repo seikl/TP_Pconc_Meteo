5870120ea8b2696f9d34c907b321f666c87c4cce
skl <skl@macbookskl.einet.ad.eivd.ch>
2014-06-02 21:13:19 +0200

    Merge origin/master
    Conflicts:
    	src/tp_pconc_meteo/CapteurTemperature.java

--- src/tp_pconc_meteo/Zone.java
+++ src/tp_pconc_meteo/Zone.java
@@ -12,18 +12,19 @@ public class Zone extends Thread {
     private double lumiereZone;
     
     private RecepteurTemperature recTempZone_;
+    private CapteurTemperature TempCapteur_;
+    private ActuateurTemperature TempActuateur_;
     private RecepteurPression recPresZone_;
     private int idZone_;
     private double etatZone_ = 0.;
-    private double infExt_ = 1.;
-    private double infInt_ = 0.5;
-    private double infSC_ = 0.5;
-    
+    private double influenceExt_ = 1.;
+    private double influcenceInt_ = 0.5;
+            
     public Zone (int idZone, double infInt, RecepteurTemperature recTempZone)
     {
         recTempZone_ = recTempZone;
         idZone_ = idZone;
-        infInt_ = infInt;
+        influcenceInt_ = infInt;
     }
     
     @Override
@@ -37,8 +38,8 @@ public class Zone extends Thread {
             if (recTempZone_.readyToWrite[idZone_]==false)
             {      
                 //mise à jour de l'etat de La Zone
-                etatZone_ = etatZone_ + infExt_ * (infExt_ - etatZone_)
-                        + infSC * (recTempZone_.getTemp() - etatZone_);
+                etatZone_ = etatZone_ + influenceExt_ * (recTempZone_.getTemp() - etatZone_)
+                        + TempCapteur_.getTemp() * ( TempActuateur_.getTempToModify() - etatZone_);
                 
                 //indique que la zone est prête à recevoir une température
                 recTempZone_.readyToWrite[idZone_]=true;
